ae6ce5c7b428f095fa18ef0b9bb4d90220aee575
bilal <abii.manyun@gmail.com>
2022-08-04 06:04:33 +0700

    update tugas convert portofolio ke ReactJS

--- src/Materi/Styling/Bootstrap/KomponenItem/Footer.jsx
+++ src/Materi/Styling/Bootstrap/KomponenItem/Footer.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import * as icon from "react-bootstrap-icons";
 
 export default class Footer extends React.Component {
   render() {
     return (
-      <footer className="bg-dark text-white text-center pb-5">
+      <footer class="bg-dark text-white text-center pb-5">
         <p>
-          Created with <i className="bi bi-suit-heart-fill text-danger" /> by{" "}
-          <a href="https://www.instagram.com/abibinyun/" className="text-white fw-bold">
+          Created with <icon.HeartFill color="red" /> by{" "}
+          <a href="https://www.instagram.com/abibinyun/" class="text-white fw-bold">
             Muhammad Bilal
           </a>
         </p>
